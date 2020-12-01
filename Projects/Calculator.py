28464b1d3043db245b0b6593a7beaa8556c8eaec
Arijeet De <arijeetde@gmail.com>
2020-12-01 17:08:25 +0530

    Addition Functionality

--- Projects/Calculator.py
+++ Projects/Calculator.py
@@ -15,6 +15,48 @@ class MyLayout(Widget):
     def clear(self):
         self.ids.calc_input.text = '0'
 
+    def number_button(self, button):
+
+        prior = self.ids.calc_input.text
+
+        if prior == '0':
+            self.ids.calc_input.text = f'{button}'
+        else:
+            self.ids.calc_input.text = f'{prior}{button}'
+
+    def add(self):
+
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}+'
+
+    def subtract(self):
+
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}-'
+
+    def multiply(self):
+
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}x'
+
+    def divide(self):
+
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}/'
+
+    def equals(self):
+
+        prior = self.ids.calc_input.text
+
+        if "+" in prior:
+            num_list = prior.split("+")
+            answer = 0
+
+            for num in num_list:
+                answer += int(num)
+
+            self.ids.calc_input.text = str(answer)
+
 
 class CalculatorApp(App):
 
